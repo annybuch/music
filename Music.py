29f8c151ec4dbfdc41aa06b2d5e60e3689d81dd7
Raiany Cristina <raianyoc@gmail.com>
2023-06-21 20:51:15 -0300

    Update Music.py

--- Music.py
+++ Music.py
@@ -4,4 +4,4 @@ pygame.mixer.init()  # inicializar os comandos do mixer.
 pygame.init()   # inicializar todos os comandos do pygame.
 pygame.mixer.music.load('justin.mp3')   # Carregar um arquivo de música mp3.
 pygame.mixer_music.play()    # Inicializar a reprodução música
-pygame.event.wait()  # esperar por um unico evendo (a musica) da fila, para encerrar a reprodução.
+pygame.event.wait()  # esperar por um unico evento da fila acabar (a musica), para encerrar a reprodução.
